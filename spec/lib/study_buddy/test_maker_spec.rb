ca1719c22bf0e5883a348d3925d5ea4e65f91016
Ryan Golden <ryan.john.golden@gmail.com>
2012-06-08 14:49:33 -0400

    DRYing up my spec file
    I had a lot of code repeated, so I moved some variable declarations up
    into a let variable.  I also moved the pending specs to the bottom to
    get them out of the way.

--- spec/lib/study_buddy/test_maker_spec.rb
+++ spec/lib/study_buddy/test_maker_spec.rb
@@ -1,49 +1,45 @@
 require_relative '../../../lib/study_buddy/test_maker'
 
 describe StudyBuddy::TestMaker do
-  let(:dummy_file){dummy_file = File.expand_path("spec/fixtures/test.txt")}
+  let(:dummy_file){ File.expand_path("spec/fixtures/test.txt") }
+  let(:opts) { {:data_file => dummy_file} }
+  let(:study_buddy_dir) { File.join(Dir.pwd, ".study_buddy") }
 
   before(:each) do
-    Dir.mkdir(File.join(Dir.pwd, ".study_buddy")) unless File.exists?(File.join(Dir.pwd, ".study_buddy"))
+    Dir.mkdir(study_buddy_dir) unless File.exists?(study_buddy_dir)
   end
 
   after(:each) do
-    FileUtils.rm_rf(File.join(Dir.pwd, ".study_buddy"))
+    FileUtils.rm_rf(study_buddy_dir)
   end
 
   it "responds correctly when there's no data in the options hash" do
-    expect {StudyBuddy::TestMaker.new({})}.to raise_error("Please specify a data file.")
+    expect { StudyBuddy::TestMaker.new({}) }.to raise_error("Please specify a data file.")
   end
 
   it "makes the test dir" do
-    opts = {:data_file => dummy_file}
     StudyBuddy::TestMaker.new(opts)
-    File.exists?(File.join(Dir.pwd, ".study_buddy", "test")).should == true
+    File.exists?(File.join(study_buddy_dir, "test")).should == true
   end
 
   it "reads the lines of the file" do
-    opts = {:data_file => dummy_file}
     StudyBuddy::TestMaker.new(opts).lines.kind_of?(Array) == true
   end
 
-  it "defaults to 10 questions when no number is indicated in the options"
-
   it "reads the number from the options hash" do
-    opts = {:number => 1, :data_file => dummy_file}
-    StudyBuddy::TestMaker.new(opts).questions.length.should == 1
+    opts[:number] = 1
+    test = StudyBuddy::TestMaker.new(opts)
+    test.questions.length.should == 1
   end
 
   it "reads the range from the options hash" do
-    opts = {:range => "2-4", :data_file => dummy_file}
-    StudyBuddy::TestMaker.new(opts).questions.length.should == 3
+    opts[:range] = "2-4"
+    test = StudyBuddy::TestMaker.new(opts)
+    test.questions.length.should == 3
   end
-
-  # I don't know how to implement this.
-  it "calls 'start' if the option is specified" 
 
   context "writing the test files" do
     before(:each) do
-      opts = {:data_file => dummy_file}
       StudyBuddy::TestMaker.new(opts)
     end
 
@@ -59,4 +55,8 @@ describe StudyBuddy::TestMaker do
       File.exists?(File.join(Dir.pwd, ".study_buddy", "test", "test_KEY")).should == true
     end
   end
+  
+  # Pending Specs
+  it "calls 'start' if the option is specified" 
+  it "defaults to 10 questions when no number is indicated in the options"
 end
